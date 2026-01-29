57d304ae63d71e74d425fd445bdcb41c895df286
Victor1890 <victorrosario.1890@gmail.com>
2026-01-29 14:05:10 -0400

    feat: implement user module with entity, service, and controller; add base entity and interfaces

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,14 +3,18 @@ import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { dataSourceOptions } from '@/database/data-source';
 import { apiEnvValidation } from '@/shared/validators/api-env-validation.schema';
+import { appModules } from '@/shared/config/modules';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      validationSchema: apiEnvValidation
+      validationSchema: apiEnvValidation,
     }),
-    TypeOrmModule.forRoot(dataSourceOptions)
+    TypeOrmModule.forRoot(dataSourceOptions),
+    ...appModules,
+    UserModule,
   ],
   // controllers: [AppController],
   // providers: [AppService],
