d018d50098d428ccc4ee12175d0ca58160f7dcfd
Victor Jesús Rosario Vásquez <46900196+Victor1890@users.noreply.github.com>
2026-01-29 12:08:31 -0400

    Feat/add db module (#2)
    * chore: restructure application by removing unused files and adding database configuration
    
    * feat(database): initialize TypeORM data source and migrations
    
    - Added data-source.ts for TypeORM configuration with MySQL.
    - Created initial migration for users and tokens tables with necessary constraints.
    - Added .keep files to maintain directory structure for migrations and seeds.
    - Introduced NodeEnvs enum for environment management.
    - Implemented Joi validation schema for API environment variables.
    - Updated tsconfig.json to support ES modules and path aliases.
    
    * chore(database): remove MYSQL_USER and MYSQL_PASSWORD from docker-compose

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { dataSourceOptions } from '@/database/data-source';
+import { apiEnvValidation } from '@/shared/validators/api-env-validation.schema';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: apiEnvValidation
+    }),
+    TypeOrmModule.forRoot(dataSourceOptions)
+  ],
+  // controllers: [AppController],
+  // providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
---
+++ backend/src/shared/config/node-env.enum.ts
@@ -0,0 +1,6 @@
+export enum NodeEnvs {
+    PRODUCTION = 'production',
+    STAGING = 'staging',
+    DEVELOPMENT = 'development',
+    LOCAL = 'local',
+}
