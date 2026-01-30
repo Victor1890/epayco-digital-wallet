389b1a4830ec81124456369503d67a3ed93d406f
Victor Jesús Rosario Vásquez <46900196+Victor1890@users.noreply.github.com>
2026-01-30 15:00:08 -0400

    Feat: update header component to use auth store for sign out functionality and remove unused session fetching logic (#15)

--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -1,43 +1,9 @@
-import { useAppSession } from '@/modules/auth/session'
 import { RootComponent } from '@/modules/core/root-component'
 import { RouterContext } from '@/router'
 import { createRootRouteWithContext } from '@tanstack/react-router'
-import { createServerFn } from '@tanstack/react-start'
 import appCss from '../styles.css?url'
-import { getRequest } from '@tanstack/react-start/server'
-
-const fetchSession = createServerFn({ method: 'GET' }).handler(async () => {
-  const request = getRequest()
-  // const session = await useAppSession()
-
-  const cookieRaw = request.headers.get('cookie')
-
-  console.log('Client Raw Cookie:', cookieRaw)
-
-  // if (!client) {
-  //   await session.update({
-  //     isLogged: false,
-  //     user: null,
-  //   })
-  //   return session.data
-  // }
-
-  // await session.update({
-  //   isLogged: !!client,
-  //   user: client,
-  // })
-
-  // return session.data
-  return undefined
-})
 
 export const Route = createRootRouteWithContext<RouterContext>()({
-  beforeLoad: async () => {
-    const session = await fetchSession()
-    return {
-      session,
-    }
-  },
   head: () => ({
     meta: [
       {
