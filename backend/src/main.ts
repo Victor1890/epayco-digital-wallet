22435b5fc56efcc3ea7a2773196b72ff704b5dea
Victor1890 <victorrosario.1890@gmail.com>
2026-01-30 10:42:39 -0400

    feat(auth): refactor environment configuration and enhance CORS setup

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,23 @@
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { AppModule } from '@/app.module';
+import { CommonEnvKey } from '@/shared/enums/common-environments.enum';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.create(AppModule, {
+    rawBody: true,
+  });
+
+  const configService = app.get(ConfigService);
+  app.enableCors({
+    origin: [configService.getOrThrow<string>(CommonEnvKey.FRONTEND_URL)],
+    credentials: true,
+  });
+
+  const PORT = configService.getOrThrow<number>(CommonEnvKey.PORT);
+
+  await app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
 }
 bootstrap();
